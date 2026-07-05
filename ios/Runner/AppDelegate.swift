fc760d3fa081667f546efe0ba8f2634f8e9ce325
daanser <daaanser@gmail.com>
2026-07-06 02:16:52 +0800

    配置 Android 签名：外部 key.properties、keystore 路径、.gitignore 规则

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import Photos
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
